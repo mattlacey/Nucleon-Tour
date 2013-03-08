e4056e016ce5d040110957e4add5162c8daaeefa
Matt Lacey <laceysnr@gmail.com>
2013-03-08 00:33:55 +0000

    First pass at hills. Distant bends not rendering correctly still and uphills have massive distortion on the stripes.

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 	Nucelon Tour - main.c
 
 	Yup, this is all in one place right now, and yup, there are very few 
-	(if any, there arne't at the time of writing) forward declarations.
+	(if any, there aren't at the time of writing) forward declarations.
 
 	I'm not intending this to be a full project, more a simple experiment
 	in this style of engine and it's really just a learning exercise
@@ -20,12 +20,14 @@
 #define X_RES (800)
 #define Y_RES (600)
 #define Y_RES_2 (Y_RES >> 1)
+#define Z_MAX (0x1000)
 #define CAM_HEIGHT (16384)
 
 SDL_Surface * screen = NULL;
 SDL_Surface * road = NULL;
 
 int keys[KEY_COUNT];
+int heightMap[Z_MAX];
 
 int init()
 {
@@ -52,6 +54,7 @@ int init()
 
 	dx = 0;
 
+	// Draw a basic road
 	for(y = 0; y < Y_RES; y++)
 	{
 		if(y & 1)
@@ -72,7 +75,26 @@ int init()
 		}
 	}
 
-	// memcpy(screen->pixels, road->pixels, X_RES * Y_RES * 4);
+	// Generate some hills
+	y = 0;
+	dy = 0;
+
+	for(i = 0; i < Z_MAX >> 2; i++)
+	{
+		heightMap[(Z_MAX - 1) - i] = heightMap[i] = dy >> 8;
+		y++;
+		dy += y >> 4;
+	}
+
+	x = heightMap[(Z_MAX >> 2) - 1] * 2;
+
+	for(i = 0; i < Z_MAX >> 2; i++)
+	{
+		// second half of the up hill
+		heightMap[(Z_MAX >> 1) + i] = heightMap[(Z_MAX >> 1) - (i + 1)] = x - heightMap[i];
+	}
+
+	for(i = 0; i < Z_MAX; i+= 32) printf("%i\n", heightMap[i]);
 
 	SDL_WM_SetCaption("Nucleon Tour!", 0);
 
@@ -82,49 +104,58 @@ int init()
 void render()
 {
 	Uint32 * src, * dst;
-	int i, x, y, yy;
+	int i, x, y, yy, yDraw;
 	int xOff, dxOff;
+	int hBase = 0;
 	static int z = 0; 
 
 	SDL_LockSurface(screen);
 
-	z++;
+	z += 4;
+
+	if(z & Z_MAX) 
+	{
+		z = 0;
+	}
+
+	printf("z = %i\n", z);
 
 	xOff = 0;
 	dxOff = 0;
 
 	yy = 0;
+	yDraw = Y_RES - 1;
 
-	for(y = Y_RES - 1; y >  1; y--)
+	for(y = Y_RES - 1; yDraw >  1; y--)
 	{
 		int dx = y - Y_RES_2;
 		int mod = 0;
+		int h = 0;
 
 		int zScr = (y == Y_RES >> 1) ? 0 : CAM_HEIGHT / (y - (Y_RES >> 1));
 
 		// road map should really only map to half the screen
 		yy += 2;
 
-		dst = (Uint32 *)screen->pixels + y * X_RES;
+		dst = (Uint32 *)screen->pixels + yDraw * X_RES;
 
 		// stops us going all the way to the horizon
 		if(zScr < (CAM_HEIGHT >> 2) && zScr > 0)
 		{
 			// printf("yy = %i, z = %i\n", yy, z);
 			
-			// stripes are 64 z-units wide
-			zScr += (z >> 1);
+			zScr += z;
 
-			if(zScr & 0x10)
+			// track is straight, left, straight, right
+		
+			// this needs to wrap for the height map 
+			zScr &= Z_MAX - 1;
+
+			// do stripes
+			if(zScr & 0x20)
 			{
 				mod = 0x00222200;
 			}
-
-			// track is straight, left, straight, right
-			if(z & 0x1000)
-			{
-				z = 0;
-			}		
 
 			if(zScr & 0x400 && zScr & 0x200)
 			{
@@ -138,17 +169,34 @@ void render()
 			{
 				xOff--;
 			}
-	
+
 			// scale down xOff before using it, we do the same with dxOff later	
 			dxOff += (xOff >> 4);
 
+			// we only want to draw if the next line as above the last drawn line
+			h = heightMap[zScr];
+
+			if(hBase == 0)
+			{
+				hBase = h;
+			}
+
+			h = yDraw - (y - (h - hBase));
+
 			src = (Uint32 *)road->pixels + (Y_RES - yy) * X_RES;
 
-			// copy the line we want 
-			// right now this is going to wrap around, but we'll clamp and fix things later
-			for(x = 0; x < X_RES; x++)
+			for(; h >= 0; h--)
 			{
-				*(dst + x) = *(src  + x + (dxOff >> 3)) - mod;
+				// copy the line we want 
+				// right now this is going to wrap around, but we'll clamp and fix things later
+
+				//printf("yDraw = %i, h = %i, hBase = %i, zScr = %i\n", yDraw, h, hBase, zScr);
+				for(x = 0; x < X_RES; x++)
+				{
+					*(dst - (X_RES * h) + x) = *(src  + x + (dxOff >> 4)) - mod;
+				}
+
+				yDraw--;
 			}
 		}
 		else
@@ -158,7 +206,9 @@ void render()
 			for(x = 0; x < X_RES; x++)
 			{
 				*(dst + x) = 0xFF999955;
-			}	
+			}
+			
+			yDraw--;
 		}
 	}
 
